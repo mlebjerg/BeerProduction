4d67e7795fd00f5d9449617e6b4c0e613776ebaa
Marc <32075278+Hejoodatnab@users.noreply.github.com>
2018-11-26 12:26:56 +0100

    Small tweaks

---
+++ Serene1/Serene1.Web/Modules/AdminLTE/BatchStarter/BatchStarterController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serene1.Modules.AdminLTE.BatchStarter
+{
+    public class BatchStarterController
+    {
+    }
+}
