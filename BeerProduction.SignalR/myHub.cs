7ed0c90d79d907546035399fb5d502835bd8fd2b
Jonathan <petjon_real@hotmail.com>
2018-12-05 15:07:41 +0100

    SignalRtest

---
+++ BeerProduction.SignalR/myHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace BeerProduction.SignalR
+{
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        //Method that sends out what data all servers should get.
+        public void BroadCastServerTime()
+        {
+            Clients.All.MessageReciever(DateTime.Now);
+        }
+    }
+}
