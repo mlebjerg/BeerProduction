f08e094a4dcd4fba2ff30262aa835613768cdf3d
Mikkel Ebjerg <mlebjerg@me.com>
2018-12-11 16:23:15 +0100

    SignalR Start

---
+++ Serene1/Serene1.Web/SubscriptionHub/SignalRStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Serene1.SubscriptionHub.SignalRStartup))]
+
+namespace Serene1.SubscriptionHub
+{
+    public class SignalRStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
--- Serene1/Serene1.Web/SubscriptionHub/SubscriptionHub.cs
+++ Serene1/Serene1.Web/SubscriptionHub/SubscriptionHub.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Serene1.SubscriptionHub
 {
+    [HubName("SubscriptionTicker")]
     public class SubscriptionHub : Hub
     {
         public void Hello()
